8c70aa7f307bd1180f5091e1a45177974eb62647
Chris <lavalle.l.c@gmail.com>
2025-07-30 13:12:02 -0500

    Generate todo list token server-side

--- backend/grouptodo/todos/views.py
+++ backend/grouptodo/todos/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import TodoList, TodoItem
@@ -9,6 +9,13 @@ class TodoListViewSet(viewsets.ModelViewSet):
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
     lookup_field = "token"
+
+    def create(self, request, *args, **kwargs):
+        """Create a new todo list with a server-generated token."""
+        todo_list = TodoList.objects.create()
+        serializer = self.get_serializer(todo_list)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     @action(detail=True, methods=["get"])
     def items(self, request, token=None):
