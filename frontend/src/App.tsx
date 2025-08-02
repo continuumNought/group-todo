c5ff01916aacd28412c16b517c56006056ebb979
Chris <lavalle.l.c@gmail.com>
2025-08-02 11:35:49 -0500

    Add button to create todo list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,34 @@
 import useStore from './store';
 
 function App() {
-  const message = useStore((state) => state.message);
-  return <h1>{message}</h1>;
+  const token = useStore((state) => state.token);
+  const setToken = useStore((state) => state.setToken);
+
+  const handleClick = async () => {
+    const response = await fetch('/api/lists/', { method: 'POST' });
+    const data = await response.json();
+    setToken(data.token);
+    window.history.pushState(null, '', `/${data.token}`);
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      {!token && (
+        <button onClick={handleClick}>Get Busy!</button>
+      )}
+      <ul>
+        <li>Under Construction</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
